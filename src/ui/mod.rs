c0460fdae57801102848472551b4a58ea1ef6b9e
guyguy2001 <guygeva2311@gmail.com>
2024-05-03 23:13:38 +0300

    Documentation

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,3 +1,7 @@
+// TODO: I don't think this module makes sense.
+// I Think I should have a folder per game section, and a ui folder in each,
+// instead of a ui folder with ui-related game states.
+
 pub mod level_transition;
 
 pub mod lose_screen;
