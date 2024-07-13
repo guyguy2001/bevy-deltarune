5ce579c369d575c4c36121bedf6d069d813fa7d5
guyguy2001 <guygeva2311@gmail.com>
2024-07-14 00:10:12 +0300

    Reorganize bullet_hell/mod.rs

--- src/bullet_hell/mod.rs
+++ src/bullet_hell/mod.rs
@@ -1,22 +1,5 @@
 use bevy::prelude::*;
 
-use self::arena::ArenaPlugin;
-use self::bullet::BulletsPlugin;
-use self::dash::DashPlugin;
-use self::debug::DebugPlugin;
-use self::effects::EffectsPlugin;
-use self::enemies::laser::LaserPlugin;
-use self::enemies::moving_cannon::MovingCannonPlugin;
-use self::game_ui::GameUIPlugin;
-use self::health::HealthPlugin;
-use self::hit_effect::HitEffectPlugin;
-use self::level::LevelPlugin;
-use self::level_end_animation::LevelEndAnimationPlugin;
-use self::level_timer::LevelTimerPlugin;
-use self::player::PlayerPlugin;
-use self::sword::SwordPlugin;
-use self::upgrades::populate_upgrades_pool;
-
 pub use self::level::{CurrentLevelConfig, LevelConfig, LevelFinishedEvent};
 
 mod effects;
@@ -43,23 +26,22 @@ pub struct BulletHellPlugin;
 impl Plugin for BulletHellPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins((
-            ArenaPlugin,
-            BulletsPlugin,
-            DashPlugin,
-            DebugPlugin,
-            EffectsPlugin,
-            GameUIPlugin,
-            HealthPlugin,
-            HitEffectPlugin,
-            LaserPlugin,
-            LevelEndAnimationPlugin,
-            LevelTimerPlugin,
-            LevelPlugin,
-            MovingCannonPlugin,
-            // PhysicsPlugin,
-            PlayerPlugin,
-            SwordPlugin,
+            arena::ArenaPlugin,
+            bullet::BulletsPlugin,
+            dash::DashPlugin,
+            debug::DebugPlugin,
+            effects::EffectsPlugin,
+            enemies::laser::LaserPlugin,
+            enemies::moving_cannon::MovingCannonPlugin,
+            game_ui::GameUIPlugin,
+            health::HealthPlugin,
+            hit_effect::HitEffectPlugin,
+            level::LevelPlugin,
+            level_end_animation::LevelEndAnimationPlugin,
+            level_timer::LevelTimerPlugin,
+            player::PlayerPlugin,
+            sword::SwordPlugin,
         ))
-        .add_systems(Startup, populate_upgrades_pool);
+        .add_systems(Startup, upgrades::populate_upgrades_pool);
     }
 }
